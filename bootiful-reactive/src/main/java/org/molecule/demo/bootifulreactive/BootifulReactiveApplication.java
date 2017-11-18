757afda6ed8d2b36f2fa52bafaf1c01a52eb58fd
dongzhuming <dongzhuming@gmail.com>
2017-11-18 21:55:19 +0800

    reactive web demo

---
+++ bootiful-reactive/src/main/java/org/molecule/demo/bootifulreactive/BootifulReactiveApplication.java
@@ -0,0 +1,12 @@
+package org.molecule.demo.bootifulreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifulReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootifulReactiveApplication.class, args);
+	}
+}

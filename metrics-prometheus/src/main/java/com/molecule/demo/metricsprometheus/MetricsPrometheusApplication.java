44cd336ee31cb01561a92e3467d6a29e981617d8
dongzhuming <dongzhuming@gmail.com>
2018-03-17 22:55:27 +0800

    spring boot Metrics - Prometheus(未成功)

---
+++ metrics-prometheus/src/main/java/com/molecule/demo/metricsprometheus/MetricsPrometheusApplication.java
@@ -0,0 +1,25 @@
+package com.molecule.demo.metricsprometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Dong Zhuming
+ */
+@SpringBootApplication
+@RestController
+public class MetricsPrometheusApplication {
+
+    @GetMapping("/hi")
+    public String sayHi() {
+        return "Hello";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetricsPrometheusApplication.class, args);
+    }
+}

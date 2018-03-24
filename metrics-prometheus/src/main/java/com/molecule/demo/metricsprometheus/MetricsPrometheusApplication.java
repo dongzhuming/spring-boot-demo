fdcb7c68347ad90fff398bd0809c92875cf14309
dongzhuming <dongzhuming@163.com>
2018-03-24 15:43:44 +0800

    spring boot metrics prometheus - add entity metrics

--- metrics-prometheus/src/main/java/com/molecule/demo/metricsprometheus/MetricsPrometheusApplication.java
+++ metrics-prometheus/src/main/java/com/molecule/demo/metricsprometheus/MetricsPrometheusApplication.java
@@ -2,12 +2,16 @@ package com.molecule.demo.metricsprometheus;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.spring.autoconfigure.MeterRegistryCustomizer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Clock;
+import java.util.stream.Stream;
 
 /**
  * @author Dong Zhuming
@@ -27,6 +31,17 @@ public class MetricsPrometheusApplication {
       return  (registry) -> registry.config().commonTags("job", "test1");
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(CityRepository cityRepository) {
+
+        return args -> {
+            System.out.println(Clock.systemDefaultZone());
+            Stream.of("li", "lv", "yu", "lu").forEach(s -> {
+                cityRepository.save(new City(s, "country"));
+            });
+        };
+
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MetricsPrometheusApplication.class, args);

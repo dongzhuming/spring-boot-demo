ae34ffee92bbb8df616224cd94d354725ccb5b2c
dongzhuming <dongzhuming@163.com>
2018-05-12 00:27:26 +0800

    spring boot metrics prometheus - add entity metrics

--- spring-data-ldap/src/main/java/org/molecule/demo/springdataldap/Person.java
+++ spring-data-ldap/src/main/java/org/molecule/demo/springdataldap/Person.java
@@ -3,5 +3,47 @@ package org.molecule.demo.springdataldap;
 /**
  * @author Dong Zhuming
  */
+
+import lombok.Data;
+
+
+import javax.naming.Name;
+
+
+import org.springframework.ldap.odm.annotations.Attribute;
+
+import org.springframework.ldap.odm.annotations.DnAttribute;
+
+import org.springframework.ldap.odm.annotations.Entry;
+
+import org.springframework.ldap.odm.annotations.Id;
+
+
+/**
+ * {@link Person} object stored inside LDAP.
+ *
+ * @author Mark Paluch
+ */
+
+@Entry(base = "ou=people,dc=springframework,dc=org", objectClasses = "inetOrgPerson")
+
+@Data
+
 public class Person {
+
+
+    private @Id
+    Name id;
+
+    private @DnAttribute(value = "uid", index = 3)
+    String uid;
+
+    private @Attribute(name = "cn")
+    String fullName;
+
+    private @Attribute(name = "sn")
+    String lastname;
+
+    private String userPassword;
+
 }
--- spring-data-ldap/src/main/java/org/molecule/demo/springdataldap/PersonRepository.java
+++ spring-data-ldap/src/main/java/org/molecule/demo/springdataldap/PersonRepository.java
@@ -1,7 +1,41 @@
 package org.molecule.demo.springdataldap;
 
+
+import java.util.List;
+
+
+import javax.naming.Name;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+
 /**
- * @author Dong Zhuming
+ * Repository interface to manage {@link Person} instances.
+ *
+ * @author Mark Paluch
  */
-public class PersonRepository {
+
+public interface PersonRepository extends CrudRepository<Person, Name> {
+
+
+    /**
+     * Find by UserId.
+     *
+     * @param uid
+     * @return
+     */
+
+    List<Person> findByUid(String uid);
+
+
+    /**
+     * Prefix search on {@link Person#getLastname()}.
+     *
+     * @param prefix
+     * @return
+     */
+
+    List<Person> findByLastnameStartsWith(String prefix);
+
 }
